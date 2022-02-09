d292c3ee8d54ccffb143f762893c1e5040be7e0c
Micaella Colombo <micaelc@g.clemson.edu>
2022-02-08 22:06:55 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Your code here
+class Polygon {
+    constructor(side) {
+        this.side = side;
+    }
+
+    get countSides() {
+        return (this.side).length
+    }
+
+    get perimeter() {
+        return this.side.reduce((accumulator, currentValue) => accumulator + currentValue);
+    }
+}
+
+class Triangle extends Polygon {
+    // inherits constructor from Polygon
+    get isValid() {
+        return (this.side[0] + this.side[1]) > this.side[2] && (this.side[1] + this.side[2]) > this.side[0]
+    }
+}
+
+class Square extends Polygon {
+    // inherits constructor from Polygon
+    get isValid() {
+        return(this.side[0] === this.side[1] && this.side[1] === this.side[2] && this.side[2] === this.side[3])
+    }
+
+    get area() {
+        return this.side[0]*this.side[0];
+    }
+}
+  
+  
+
+
+  
